b2b5bf6908d084527b2d86f582201c5e11cd67e7
Jamshadj <jamshadalij2004@gmail.com>
2025-03-20 14:40:56 +0530

    build error fixed

---
+++ src/types/react-lottie.d.ts
@@ -0,0 +1,25 @@
+declare module 'react-lottie' {
+  interface LottieOptions {
+    loop?: boolean;
+    autoplay?: boolean;
+    animationData: any;
+    rendererSettings?: {
+      preserveAspectRatio?: string;
+    };
+  }
+
+  interface LottieProps {
+    options: LottieOptions;
+    height?: number | string;
+    width?: number | string;
+    isStopped?: boolean;
+    isPaused?: boolean;
+    eventListeners?: Array<{
+      eventName: string;
+      callback: () => void;
+    }>;
+  }
+
+  const Lottie: React.FC<LottieProps>;
+  export default Lottie;
+} 
