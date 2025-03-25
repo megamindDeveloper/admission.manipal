2a0bb4b3e66042e7dadd4e482acfdd6f084e83ba
Jamshadj <jamshadalij2004@gmail.com>
2025-03-25 09:55:55 +0530

    whatsapp widget added

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { Toaster } from "react-hot-toast";
+import WhatsappChatWidget from "./components/WhatsappChatWidget/WhatsappChatWidget";
 const geistSans = Geist({
   variable: "--font-geist-sans",
   subsets: ["latin"],
@@ -63,6 +64,7 @@ export default function RootLayout({
         {" "}
         <Toaster position="top-right" />
         {children}
+        <WhatsappChatWidget />
       </body>
     </html>
   );
